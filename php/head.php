23456b7adb934a3dfeb644682dc45715ef5c306b
Jamilli Gioielli <65102673+jamilligioielli@users.noreply.github.com>
2021-03-08 01:06:13 -0300

    Atualização final

---
+++ php/head.php
@@ -0,0 +1,14 @@
+
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" />
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <link rel="preconnect" href="https://fonts.gstatic.com" />
+  <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro&display=swap" rel="stylesheet" />
+  <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@600&family=Playfair+Display:wght@800&family=Roboto&display=swap" rel="stylesheet" />
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+  <link rel="sortcut icon" href="./img/polvo.png" type="image/png" style="object-fit: contain;" />;
+  <script src="https://kit.fontawesome.com/6726683f80.js" crossorigin="anonymous"></script>
+  
